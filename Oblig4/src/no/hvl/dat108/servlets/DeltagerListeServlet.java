a5414c91ecea4ccfaa866b002c380599ecc83fc4
h131177 <h131177@stud.hvl.no>
2020-11-07 23:55:25 +0100

    Fikset uautorisert tilgang deltagerliste

--- Oblig4/src/no/hvl/dat108/servlets/DeltagerListeServlet.java
+++ Oblig4/src/no/hvl/dat108/servlets/DeltagerListeServlet.java
@@ -1,11 +1,16 @@
 package no.hvl.dat108.servlets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import no.hvl.dat108.hjelpeklasser.InnloggingUtil;
+import no.hvl.dat108.model.Deltager;
 
 /**
  * Servlet implementation class DeltagerListeServlet
@@ -14,17 +19,22 @@ import javax.servlet.http.HttpServletResponse;
 public class DeltagerListeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-  
+	List<Deltager> liste;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		request.getRequestDispatcher("WEB-INF/jsp/deltagerliste.jsp").forward(request,response);
+		if(InnloggingUtil.isInnlogget(request)) {
+			//TODO hente alle deltagere fra database
+			//liste = deltakerEAO.hentAlle();
+			//TODO sortere listen
+			//TODO finne innlogget bruker
+			//request.getSession().setAttribute("deltakerliste", liste);
+			request.getRequestDispatcher("WEB-INF/jsp/deltagerliste.jsp").forward(request,response);
+		} else {
+			String feilmelding = "Det er kun registrerte deltagere som får se deltagerlisten. "
+					+ "Logg inn ved å gi mobilnummer og passord";
+			request.getSession().setAttribute("melding", feilmelding);
+			request.getRequestDispatcher("logginn").forward(request, response);
+		}
 	}
-
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		response.sendRedirect("ferdig");
-	}
-
 }
