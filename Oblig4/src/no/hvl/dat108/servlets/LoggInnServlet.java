5f55d756b66fbb06b12608dba1f855d1f5d4b86c
h131177 <h131177@stud.hvl.no>
2020-11-08 21:55:29 +0100

    Innlogging fikset

--- Oblig4/src/no/hvl/dat108/servlets/LoggInnServlet.java
+++ Oblig4/src/no/hvl/dat108/servlets/LoggInnServlet.java
@@ -10,45 +10,55 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import no.hvl.dat108.hjelpeklasser.InnloggingUtil;
+import no.hvl.dat108.hjelpeklasser.PassordUtil;
 import no.hvl.dat108.hjelpeklasser.Validation;
 import no.hvl.dat108.model.Deltager;
 import no.hvl.dat108.model.DeltagerEAO;
 
-
 @WebServlet(name = "LogginnServlet", urlPatterns = { "/logginn" })
 public class LoggInnServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Validation val = new Validation();
-	
+	private PassordUtil passordUtil = new PassordUtil();
+
 	@EJB
-	private DeltagerEAO deltagerEAO = new DeltagerEAO();
- 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//String melding = (String) request.getSession().getAttribute("melding");	
+	DeltagerEAO deltagerEAO = new DeltagerEAO();
+	Deltager deltager = new Deltager();
+	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// String melding = (String) request.getSession().getAttribute("melding");
 		request.getRequestDispatcher("WEB-INF/jsp/logginn.jsp").forward(request, response);
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String mobil = request.getParameter("mobil");
 		String passord = request.getParameter("passord");
 		mobil = val.escapeHtml(mobil);
 		passord = val.escapeHtml(passord);
-		
-		Deltager deltager = deltagerEAO.finnEnDeltager(mobil);
-		
-		if(val.erGyldigMobil(mobil)) {
-			//Sjekker om mobil finnes i database og krypterer passord dersom ja
+		String kryptertLagretPassord = "";
+		boolean testMobil = val.erGyldigMobil(mobil);
+		boolean testPassord = val.erGyldigPassord(passord);
+
+		if (testMobil) {
+			deltager = deltagerEAO.finnEnDeltager(mobil);
+			if (deltager == null) {
+				testMobil = false;
+			} else {
+				kryptertLagretPassord = deltager.getPassordhash();
+				// testPassord = passordUtil.sjekkPassord(passord, kryptertLagretPassord);
+				// InnloggingUtil.isGyldigPassord(passord, kryptertLagretPassord) ALTERNATIV
+			}
 		}
-		//TODO hente kryptert passord fra database
-		if(InnloggingUtil.isGyldigPassord(passord, "pass")) {
+		if (testMobil && testPassord) {
 			InnloggingUtil.loggInnMedTimeout(request, 120);
-			
-			request.getSession().setAttribute("innlogger", deltager);
-			
+
+			request.getSession().setAttribute("innlogget", deltager);
 			response.sendRedirect("deltagerliste");
 		} else {
 			String feilmelding = "Ugyldig brukernavn og/eller passord";
